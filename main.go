fc29fddebc227a258a33c93f8e41d545c1b018e1
andfxx27 <idea.andersen@gmail.com>
2022-06-18 11:37:39 +0700

    feat: heroku deploy preparation

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/andfxx27/go-multiplication-table/src/domain"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	http.HandleFunc("/", domain.IndexHandler)
 
-	log.Println("Application started on :4000...")
-	log.Fatalln(http.ListenAndServe("localhost:4000", nil))
+	port := os.Getenv("PORT")
+	log.Printf("Application started on :%v", port)
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
